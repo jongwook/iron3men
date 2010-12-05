c47202ad56a9f52e4ab7a3839ac2b4cbcd40d854
Jong Wook Kim <ilikekjw@gmail.com>
2010-12-04 21:09:43 -0500

    initial commit

---
+++ lib/sample/Release/sample.d
@@ -0,0 +1,6 @@
+/home/jongwook/Downloads/llvm-2.7/projects/sample/lib/sample/Release/sample.o \
+ /home/jongwook/Downloads/llvm-2.7/projects/sample/lib/sample/Release/sample.d: \
+ sample.c \
+ /home/jongwook/Downloads/llvm-2.7/projects/sample/include/sample.h
+
+/home/jongwook/Downloads/llvm-2.7/projects/sample/include/sample.h:
---
+++ tools/sample/Release/main.d
@@ -0,0 +1,6 @@
+/home/jongwook/Downloads/llvm-2.7/projects/sample/tools/sample/Release/main.o \
+ /home/jongwook/Downloads/llvm-2.7/projects/sample/tools/sample/Release/main.d: \
+ main.c \
+ /home/jongwook/Downloads/llvm-2.7/projects/sample/include/sample.h
+
+/home/jongwook/Downloads/llvm-2.7/projects/sample/include/sample.h:
