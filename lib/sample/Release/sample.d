28e571234d0d4e8785c4ca8f684e8c79084bc9f3
Dong Young Yoon <dyoon@umich.edu>
2010-12-17 23:32:27 -0500

    Superblock formulation working for single loop

--- lib/sample/Release/sample.d
+++
@@ -1,6 +0,0 @@
-/home/jongwook/Downloads/llvm-2.7/projects/sample/lib/sample/Release/sample.o \
- /home/jongwook/Downloads/llvm-2.7/projects/sample/lib/sample/Release/sample.d: \
- sample.c \
- /home/jongwook/Downloads/llvm-2.7/projects/sample/include/sample.h
-
-/home/jongwook/Downloads/llvm-2.7/projects/sample/include/sample.h:
--- tools/sample/Release/main.d
+++
@@ -1,6 +0,0 @@
-/home/jongwook/Downloads/llvm-2.7/projects/sample/tools/sample/Release/main.o \
- /home/jongwook/Downloads/llvm-2.7/projects/sample/tools/sample/Release/main.d: \
- main.c \
- /home/jongwook/Downloads/llvm-2.7/projects/sample/include/sample.h
-
-/home/jongwook/Downloads/llvm-2.7/projects/sample/include/sample.h:
